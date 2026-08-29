743913a99fd8a2484c63915697b38bdee139971c
flashpoint-jpg <fclipinho@gmail.com>
2026-08-28 23:13:33 -0300

    Add files via upload

--- entregaflash-sw.js
+++ entregaflash-sw.js
@@ -1,6 +1,6 @@
 // Entrega Flash - Service Worker com atualização forçada
-// Versão: 20260829-3
-const EF_VERSION = '20260829-3';
+// Versão: 20260829-5
+const EF_VERSION = '20260829-5';
 const EF_HOME = './index.html?v=' + EF_VERSION;
 
 self.addEventListener('install', (event) => {
