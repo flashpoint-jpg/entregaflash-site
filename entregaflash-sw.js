f1c06fede14eee02a28e96c222a35bffea309fdc
flashpoint-jpg <fclipinho@gmail.com>
2026-09-06 21:26:16 -0300

    Add files via upload

--- entregaflash-sw.js
+++ entregaflash-sw.js
@@ -1,7 +1,7 @@
-// build: entregaflash-aprovacao-realtime-20260906-v1
+// build: entregaflash-funil-anuncio-20260906-v1
 // Entrega Flash - Service Worker com atualização forçada
-// Versão: 20260906-APROVACAO-REALTIME-1
-const EF_VERSION = '20260906-APROVACAO-REALTIME-1';
+// Versão: 20260906-FUNIL-ANUNCIO-1
+const EF_VERSION = '20260906-FUNIL-ANUNCIO-1';
 const EF_HOME = './index.html?v=' + EF_VERSION;
 
 self.addEventListener('install', (event) => {
