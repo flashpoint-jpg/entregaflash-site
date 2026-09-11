beb32918927be774cf7aea721db99339654aad6a
flashpoint-jpg <fclipinho@gmail.com>
2026-09-11 10:50:11 -0300

    Add files via upload

--- entregaflash-sw.js
+++ entregaflash-sw.js
@@ -1,6 +1,6 @@
-// build: 20260911-LIDER-CIDADE-2
+// build: 20260911-LIDER-CIDADE-3
 // Entrega Flash - Service Worker com atualização forçada + reparo de Push + integração Vendaí
-const EF_VERSION = '20260911-LIDER-CIDADE-2';
+const EF_VERSION = '20260911-LIDER-CIDADE-3';
 const EF_HOME = './index.html?v=' + EF_VERSION;
 const EF_PUSH_REPAIR = '/push-repair.js?v=' + EF_VERSION;
 const EF_PUSH_RAIO = '/push-despacho-raio.js?v=' + EF_VERSION;
