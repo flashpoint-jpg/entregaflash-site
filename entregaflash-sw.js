db83373229bb33811cb64b515dea7158f487d4ef
flashpoint-jpg <fclipinho@gmail.com>
2026-09-22 15:01:40 -0300

    Force Entrega Flash login/online fix update

--- entregaflash-sw.js
+++ entregaflash-sw.js
@@ -1,6 +1,6 @@
-// build: 20260920-ATUALIZACAO-OBRIGATORIA-1
+// build: 20260922-LOGIN-ONLINE-FIX-1
 // Entrega Flash - Service Worker com atualização forçada + reparo de Push + integração Vendaí
-const EF_VERSION = '20260920-ATUALIZACAO-OBRIGATORIA-1';
+const EF_VERSION = '20260922-LOGIN-ONLINE-FIX-1';
 const EF_HOME = './index.html?v=' + EF_VERSION;
 const EF_PUSH_REPAIR = '/push-repair.js?v=' + EF_VERSION;
 const EF_PUSH_RAIO = '/push-despacho-raio.js?v=' + EF_VERSION;
